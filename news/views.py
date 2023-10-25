086ee8f1426cafba31e4fd20dcfc409f0e933b53
bugaj55113 <bbugi2016@gmail.com>
2023-10-25 22:17:29 +0200

    Added news application and 5-column model

--- news/views.py
+++ news/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 def index(request):
-    return HttpResponse("News")
+    news = News.objects.order_by('-create_time')
+
+    context = {'news': news}
+
+    return render(request, 'news/index.html', context)
 # Create your views here.
