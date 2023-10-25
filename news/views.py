d10db3ef4c8821fecfcc17a812b3eec030a58646
bugaj55113 <bbugi2016@gmail.com>
2023-10-25 16:07:09 +0200

    Two basic applications have been added

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-def index(request):
-    return render(request, 'index.html')
 
+def index(request):
+    return HttpResponse("News")
 # Create your views here.
