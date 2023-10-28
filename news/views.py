4c69df2610656d12782267e8784f2669640402b2
bugaj55113 <bbugi2016@gmail.com>
2023-10-28 15:08:33 +0200

    Adding the ability to add data to the database based on the created form.

--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import News
+from .forms import NewsForm
+from django.utils import timezone
 
 def index(request):
     news = News.objects.order_by('-create_time')
@@ -8,4 +10,25 @@ def index(request):
     context = {'news': news}
 
     return render(request, 'news/index.html', context)
+
+def add(request):
+    if request.method == 'POST':
+        news = NewsForm(request.POST) #Do news przypisywany jest tu obiekt formularza
+
+        if news.is_valid():
+            news = news.save(commit=False) #Do news przypisywany jest tu obiekt modelu
+
+            news.create_time = timezone.now()
+            news.last_edit_time = timezone.now()
+            news.save()
+            return redirect('view_news')
+        
+        else:
+            context = {'form': news}
+            return render(request, 'news/add.html', context)
+        
+    else:
+        news = NewsForm()
+        context = {'form': news}
+        return render(request, 'news/add.html', context)    
 # Create your views here.
